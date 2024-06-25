4c80bb143c4be406d858c049eb61e00962f9a615
Adhim Niokagi <niokagi@gmail.com>
2024-06-26 03:23:15 +0700

    Nights

---
+++ src/components/react/Hero.jsx
@@ -0,0 +1,109 @@
+import avatar from "../../../public/assets/adhim-niokagi.jpg";
+
+function Hero() {
+  const mediaSocial = [
+    {
+      name: "linkedin",
+      icon: "bx bxl-linkedin",
+      url: "https://linkedin.com/in/adhim-niokagi",
+    },
+    {
+      name: "instagram",
+      icon: "bx bxl-instagram",
+      url: "https://instagram.com/nioka.o",
+    },
+    {
+      name: "github",
+      icon: "bx bxl-github",
+      url: "https://github.com/nioka666",
+    },
+    {
+      name: "whatsapp",
+      icon: "bx bxl-whatsapp",
+      url: "https://whatsapp.me/6287755588990",
+    },
+  ];
+
+  return (
+    <>
+      {/* Hero */}
+      <section className="hero">
+        <section className="social-media">
+          {mediaSocial.map((data) => (
+            <a href={data.url} key={data.name}>
+              <i
+                className={`${data.icon} text-2xl cursor-pointer`}
+                key={data.name}
+              ></i>
+            </a>
+          ))}
+        </section>
+        <section className="headline">
+          <h1 className="text-[50px] font-semibold">M. Adhim Niokagi</h1>
+          <h6 className="text-xl"># Front-End Developer</h6>
+          <p className="text-gray-500 mt-5 w-[91%]">
+            I'm a Front-End Web Developer based in Indonesian, and I'm very
+            passionate and dedicated to my work.
+          </p>
+          <div className="mt-10">
+            <button className="py-[14px] px-7 bg-black text-white rounded-2xl text-lg font-medium">
+              Say Hello <i className="uil uil-location-arrow ms-2"></i>
+            </button>
+          </div>
+        </section>
+        <picture>
+          <img src={avatar.src} alt="Adhim Niokagi" className="headline-img" />
+        </picture>
+      </section>
+      {/* Hero sm */}
+      <section className="sm-hero">
+        <div className="flex justify-between px-16 items-center mt-20">
+          <section className="social-media">
+            {mediaSocial.map((data) => (
+              <a href={data.url} key={data.name}>
+                <i
+                  className={`${data.icon} text-2xl cursor-pointer`}
+                  key={data.name}
+                ></i>
+              </a>
+            ))}
+          </section>
+          <picture>
+            <img
+              src={avatar.src}
+              alt="Adhim Niokagi"
+              className="w-[280px] rounded-tr-[150px] rounded-tl-[150px] rounded-br-[100px] rounded-bl-[150px] border-[12px]"
+            />
+          </picture>
+        </div>
+        {/* headline */}
+        <section className=" px-7 mt-7 flex flex-col ms-7">
+          <h1 className="text-[40px] font-semibold">M. Adhim Niokagi</h1>
+          <h6 className="text-xl max-sm:text-lg"># Front-End Developer</h6>
+          <p className="max-sm:text-sm text-gray-500 mt-5 w-[91%]">
+            I'm a Front-End Web Developer based in Indonesian, and I'm very
+            passionate and dedicated to my work.
+          </p>
+          <div className="mt-10 flex justify-between">
+            <button className="py-[14px] px-7 bg-black text-white rounded-2xl text-lg max-sm:text-base font-medium">
+              Say Hello <i className="uil uil-location-arrow ms-2"></i>
+            </button>
+            <button className="px-7 flex items-center gap-1 bg-transparent text-black rounded-2xl text-base font-normal hover:translate-y-1 transition-all">
+              <i className="bx bx-mouse text-3xl text-gray-600"></i>Scroll Down
+              <i className="bx bx-down-arrow-alt text-xl ms-1 text-gray-600"></i>
+            </button>
+          </div>
+        </section>
+      </section>
+      {/* scroll button */}
+      <section className="ms-[135px] mt-14 max-sm:hidden">
+        <button className="flex items-center gap-1 bg-transparent text-black rounded-2xl text-base font-normal hover:translate-y-1 transition-all">
+          <i className="bx bx-mouse text-3xl text-gray-600"></i>Scroll Down
+          <i className="bx bx-down-arrow-alt text-xl ms-1 text-gray-600"></i>
+        </button>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
