43b2f3d44f83a8585e653bbe973944d08411696a
Adhim Niokagi <niokagi@gmail.com>
2024-09-08 17:43:18 +0700

    adjusts

--- src/components/react/AboutMe.jsx
+++ src/components/react/AboutMe.jsx
@@ -3,7 +3,10 @@ import avatar from "../../assets/adhim_3hp.jpeg";
 function AboutMe() {
   return (
     <>
-      <div id="about" className="about-me mt-36 lg:px-16  rounded-3xl py-14 bg-gradient-to-b from-gray-100 to-white max-md:mx-4">
+      <div
+        id="about"
+        className="about-me mt-36 rounded-3xl bg-gradient-to-b from-gray-100 to-white py-14 max-md:mx-4 lg:px-16"
+      >
         <section className="text-center">
           <h1 className="text-2xl font-semibold text-black sm:text-2xl md:text-4xl">
             About Me
@@ -26,19 +29,20 @@ function AboutMe() {
           <div className="flex-col max-sm:mt-7 max-sm:px-5 sm:px-10 md:px-0 lg:w-[1500px]">
             {/* boxes */}
             <div className="flex flex-wrap gap-4 max-lg:my-7 max-lg:justify-center max-sm:justify-center max-sm:gap-2">
-              <div className="rounded-[26px] border shadow-sm px-6 py-5 text-center max-sm:px-[17px] max-sm:py-3">
+              <div className="rounded-[26px] px-6 py-5 text-center max-sm:px-[17px] max-sm:py-3 md:border md:shadow-sm">
+                {/* <i className="bx bx-code-block text-2xl"></i> */}
                 <h1 className="text-3xl font-bold max-sm:text-[21px]">1 +</h1>
                 <p className="mt-0 text-sm text-gray-500 max-sm:text-[11px]">
                   Years <br /> Experience
                 </p>
               </div>
-              <div className="rounded-[26px] border shadow-sm px-6 py-5 text-center max-sm:px-4 max-sm:py-3">
+              <div className="rounded-[26px] px-6 py-5 text-center max-sm:px-4 max-sm:py-3 md:border md:shadow-sm">
                 <h1 className="text-3xl font-bold max-sm:text-[21px]"> 4</h1>
                 <p className="mt-0 text-sm text-gray-500 max-sm:text-[11px]">
                   Completed <br /> Projects
                 </p>
               </div>
-              <div className="rounded-[26px] border shadow-sm px-6 py-5 text-center max-sm:px-3 max-sm:py-3">
+              <div className="rounded-[26px] px-6 py-5 text-center max-sm:px-3 max-sm:py-3 md:border md:shadow-sm">
                 <h1 className="text-3xl font-bold max-sm:text-[21px]">14</h1>
                 <p className="mt-0 text-sm text-gray-500 max-sm:text-[11px]">
                   License <br /> Certifications
