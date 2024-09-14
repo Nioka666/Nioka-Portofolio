15bd57535c7247771339c8609a1fa67f261ed1ea
Adhim Niokagi <niokagi@gmail.com>
2024-09-15 01:16:58 +0700

    minimizing

--- dist/_astro/PartTitle.DLkvieqg.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./jsx-runtime.lLJip256.js";function m({title:e,desc:x}){return t.jsxs("section",{className:"text-center",children:[t.jsx("h1",{className:"text-2xl font-semibold text-black sm:text-2xl md:text-4xl",children:e}),t.jsx("h3",{className:"mt-[13.4px] text-sm text-gray-400 max-sm:mt-0 md:text-[17px]",children:x})]})}export{m as P};
