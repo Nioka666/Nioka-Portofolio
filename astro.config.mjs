8e980fc864562a143ca70af7f9cb98cdc2035fca
Adhim Niokagi <niokagi@gmail.com>
2024-09-15 01:25:01 +0700

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  // output: "server",
-  // adapter: netlify()
+  output: "server",
+  adapter: vercelServerless({
+    edgeMiddleware: true,
+  }),
 });
