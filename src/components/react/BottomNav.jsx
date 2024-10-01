a5306ab26b6701088803f1b394155856d24d18e8
Adhim Niokagi <niokagi@gmail.com>
2024-10-01 23:01:04 +0700

    adjust

--- src/components/react/BottomNav.jsx
+++ src/components/react/BottomNav.jsx
@@ -50,8 +50,8 @@ function BottomNav() {
   function IconGroup() {
     return (
       <div className="icon-group flex items-center gap-5 sm:gap-7">
-        <div className="swap swap-rotate mt-[-0.5px]">
-          <input id="toogletheme" aria-label="toogletheme" type="checkbox" onChange={toggleTheme} />
+        <label className="swap swap-rotate mt-[-0.5px]" htmlFor="toggletheme">
+          <input id="toggletheme" aria-label="toggletheme" type="checkbox" onChange={toggleTheme} />
           <svg
             className={`dark:fill-textdark3 ${themeNow === "light" ? "swap-on" : "swap-off"} h-[21.5px] w-[21.5px] fill-gray-600`}
             xmlns="http://www.w3.org/2000/svg"
@@ -66,7 +66,7 @@ function BottomNav() {
           >
             <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
-        </div>
+        </label>
         <button
           ref={toggleButtonRef}
           onClick={() => setDrawerOpen((prev) => !prev)}
--- src/components/react/PartTitle.jsx
+++ src/components/react/PartTitle.jsx
@@ -1,7 +1,7 @@
 function PartTitle({ title, desc }) {
   return (
     <section className="text-center">
-      <h1 className="text-2xl font-bold text-black dark:text-textdark sm:text-2xl md:text-4xl">
+      <h1 className="text-2xl font-semibold text-black dark:text-textdark sm:text-2xl md:text-4xl">
         {title}
       </h1>
       <h2 className="mt-[13.4px] text-sm text-gray-400 max-sm:mt-0 md:text-[17px]">
