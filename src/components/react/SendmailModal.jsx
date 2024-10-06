e5c37c16108619b5d807ea541c9119cbeb3c5c23
Adhim Niokagi <niokagi@gmail.com>
2024-10-06 22:44:58 +0700

    adjust

--- src/components/react/SendmailModal.jsx
+++ src/components/react/SendmailModal.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 const SendmailModal = ({ id }) => {
   const formRef = useRef();
@@ -24,38 +24,39 @@ const SendmailModal = ({ id }) => {
 
   return (
     <>
-      <dialog id={id} className="modal">
-        <div className="modal-box grid gap-4 p-10 px-12">
-          <h3 className="text-xl font-semibold">Say Hello to Nioka</h3>
+      <dialog id={id} className="modal w-full">
+        <div className="modal-box p-10 px-12 max-sm:w-[94%] max-sm:p-7">
+          <div className="modal-head text-center">
+            <h3 className="text-xl font-semibold max-sm:text-lg">
+              Say Hello ?
+            </h3>
+            <p className="mb-4 mt-1 text-xs font-normal text-gray-400">
+              give nioka some message.
+            </p>
+          </div>
           <form
             ref={formRef}
             onSubmit={sendEmail}
             className="form-control grid w-full gap-3"
           >
-            <label className="input input-bordered flex items-center gap-2">
-              Name
-              <input
-                type="text"
-                name="user_name"
-                className="grow"
-                placeholder="Your name"
-                required
-              />
-            </label>
+            <input
+              type="text"
+              name="user_name"
+              className="input input-bordered grow max-sm:input-md"
+              placeholder="type your name"
+              required
+            />
             <textarea
               name="message"
-              className="textarea textarea-bordered"
+              className="textarea textarea-bordered h-[80px] max-sm:text-sm"
               placeholder="Type your message here"
               required
             ></textarea>
-            <button type="submit" className="btn btn-neutral mt-5">
+            <button type="submit" className="btn btn-neutral mt-3 rounded-xl">
               Send
             </button>
           </form>
         </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>Close</button>
-        </form>
       </dialog>
     </>
   );
