56f701bcbf432bf871b9befedcec3683d7f26aac
Adhim Niokagi <niokagi@gmail.com>
2024-10-09 03:03:48 +0700

    adjust

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,10 +15,5 @@ export default defineConfig({
   //     expiration: 60 * 60 * 24,
   //   },
   // }),
-  adapter: vercel({
-    imageService: true,
-    imagesConfig: {
-      sizes: [320, 640, 1280],
-    },
-  }),
+  adapter: vercel(),
 });
