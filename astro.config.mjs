479729234c01ff13462c42ed95fd06471e7fc42d
Adhim Niokagi <niokagi@gmail.com>
2024-10-09 01:40:14 +0700

    change to static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercelServerless from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: "https://nioka.vercel.app",
   integrations: [react(), tailwind()],
-  output: "server",
+  output: "static",
   adapter: vercelServerless({
     isr: {
       expiration: 60 * 60 * 24,
