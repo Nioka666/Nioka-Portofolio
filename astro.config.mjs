ccffc57a3f230e4ad8a22abc4d453685de5e3a9a
Adhim Niokagi <niokagi@gmail.com>
2024-10-09 02:47:49 +0700

    adjust

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,22 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/static';
 import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nioka.vercel.app",
   integrations: [react(), tailwind(), sitemap()],
-  output: "server",
-  adapter: vercelServerless({
-    isr: {
-      expiration: 60 * 60 * 24,
+  output: "static",
+  // adapter: vercel({
+  //   isr: {
+  //     expiration: 60 * 60 * 24,
+  //   },
+  // }),
+  adapter: vercel({
+    imagesConfig: {
+      sizes: [320, 640, 1280],
     },
   }),
 });
