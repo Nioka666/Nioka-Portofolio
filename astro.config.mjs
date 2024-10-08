3eb0017414c6e4e86b4eb049e7eabc48f50d24b8
Adhim Niokagi <niokagi@gmail.com>
2024-10-09 01:25:00 +0700

    adjust

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nioka.vercel.app",
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: vercelServerless({
-    isr: {
-      expiration: 60 * 60 * 24,
-    },
-  }),
+  output: "static",
+  // adapter: vercelServerless({
+  //   isr: {
+  //     expiration: 60 * 60 * 24,
+  //   },
+  // }),
 });
