b22f2d0c2911d970fc6520084f0f1ba02eae32cf
Adhim Niokagi <niokagi@gmail.com>
2024-10-09 02:24:57 +0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nioka.vercel.app",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   output: "server",
   adapter: vercelServerless({
     isr: {
