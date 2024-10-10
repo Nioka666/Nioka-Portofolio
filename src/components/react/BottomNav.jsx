9d37db892964a7ac041e9098dd0a5ce752f08124
Adhim Niokagi <niokagi@gmail.com>
2024-10-11 01:05:29 +0700

    adjust

--- src/components/react/BottomNav.jsx
+++ src/components/react/BottomNav.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useSwipeable } from "react-swipeable";
 import validPages from "../../data/valid_pages.json";
+import dataMenu from "../../data/nav_anchors.json";
 
 
 function BottomNav() {
@@ -61,39 +62,6 @@ function BottomNav() {
       </div>
     );
   }
-
-  const dataMenu = [
-    {
-      name: "About",
-      icon: "bx bx-user text-3xl",
-      url: "#about",
-      notFoundURL: "https://nioka.vercel.app/#about"
-    },
-    {
-      name: "Skills",
-      icon: "bx bx-universal-access text-3xl",
-      url: "#skills",
-      notFoundURL: "https://nioka.vercel.app/#skills"
-    },
-    {
-      name: "Qualifies",
-      icon: "bx bx-file text-3xl",
-      url: "#qualification",
-      notFoundURL: "https://nioka.vercel.app/#qualification"
-    },
-    {
-      name: "portfolio",
-      icon: "bx bx-image text-3xl",
-      url: "#portfolio",
-      notFoundURL: "https://nioka.vercel.app/#portfolio"
-    },
-    {
-      name: "Contact",
-      icon: "uil uil-user-square text-3xl",
-      url: "#contact",
-      notFoundURL: "https://nioka.vercel.app/#contact"
-    },
-  ];
 
   useEffect(() => {
     let currentTheme = localStorage.getItem("theme");
--- src/components/react/Navbar.jsx
+++ src/components/react/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import validPages from "../../data/valid_pages.json";
+import menuData from "../../data/nav_anchors.json";
 
 function Navbar() {
   let [is404, setIs404] = useState(false);
@@ -8,39 +9,6 @@ function Navbar() {
   const [lastScrollY, setLastScrollY] = useState(0);
   const [themeNow, setThemeNow] = useState("light");
   const webUrl = "https://nioka.vercel.app/";
-
-  const menuData = [
-    {
-      name: "About",
-      icon: "bx bx-user text-3xl",
-      url: "#about",
-      notFoundURL: "https://nioka.vercel.app/#about"
-    },
-    {
-      name: "Skills",
-      icon: "bx bx-universal-access text-3xl",
-      url: "#skills",
-      notFoundURL: "https://nioka.vercel.app/#skills"
-    },
-    {
-      name: "Qualifies",
-      icon: "bx bx-file text-3xl",
-      url: "#qualification",
-      notFoundURL: "https://nioka.vercel.app/#qualification"
-    },
-    {
-      name: "portfolio",
-      icon: "bx bx-image text-3xl",
-      url: "#portfolio",
-      notFoundURL: "https://nioka.vercel.app/#portfolio"
-    },
-    {
-      name: "Contact",
-      icon: "uil uil-user-square text-3xl",
-      url: "#contact",
-      notFoundURL: "https://nioka.vercel.app/#contact"
-    },
-  ];
 
   function toggleTheme() {
     const currentTheme = localStorage.getItem("theme");
