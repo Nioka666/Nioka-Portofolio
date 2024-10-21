03b282bd2042d674be9732b05bb0a199ebc3903f
Adhim Niokagi <niokagi@gmail.com>
2024-10-21 18:10:27 +0700

    adjust

--- src/components/react/PartTitle.jsx
+++ src/components/react/PartTitle.jsx
@@ -1,10 +1,10 @@
 function PartTitle({ title, desc }) {
   return (
     <section className="text-left px-6">
-      <h1 className="text-xl font-normal text-niodark2 dark:text-textdark2 sm:text-2xl md:text-4xl">
-        ##
+      <h1 className="text-sm font-normal text-textdark2 dark:text-textdark2 md:text-xl">
+        #{desc}
       </h1>
-      <h2 className="mt-[10px] text-xl text-niodark2 dark:text-textdark2 max-sm:mt-0 md:text-[20px]">
+      <h2 className="mt-[10px] text-3xl font-semibold text-niodark2 dark:text-textdark2 max-sm:mt-1 md:text-5xl">
         {title}
       </h2>
     </section>
