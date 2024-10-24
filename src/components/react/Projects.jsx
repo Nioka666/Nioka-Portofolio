1870ce8a0d28bf000897374d06f2210137dffe84
Adhim Niokagi <niokagi@gmail.com>
2024-10-24 17:39:45 +0700

    adjust

--- src/components/react/Projects.jsx
+++ src/components/react/Projects.jsx
@@ -10,30 +10,24 @@ function Projects() {
             $_003
           </h1>
           <h2 className={`mt-[5px] text-[28px] font-semibold text-niodark2 dark:text-textdark max-sm:mt-0 md:text-5xl`}>
-            LATEST_ PROJECT
+            Latest_ Projects
           </h2>
         </section>
-        <div className="box-wrapper h-[200px] w-[100%] rounded-2xl bg-white dark:bg-niodark2 p-4">
-          <div className="flex flex-col gap-4">
-            <div className="skeleton dark:bg-niodark3 h-24"></div>
-            <div className="skeleton dark:bg-niodark3 h-3"></div>
-            <div className="skeleton dark:bg-niodark3 h-3"></div>
+        <div className="box-wrapper h-[180px] w-[100%] rounded-3xl bg-transparent border-2 border-base-300 dark:bg-niodark2 p-4 flex items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-textdark4">001</h1>
           </div>
           <div className="tags-section"></div>
         </div>
-        <div className="box-wrapper h-[200px] w-[100%] rounded-2xl bg-white dark:bg-niodark2 p-4">
-          <div className="flex flex-col gap-4">
-            <div className="skeleton dark:bg-niodark3 h-24"></div>
-            <div className="skeleton dark:bg-niodark3 h-3"></div>
-            <div className="skeleton dark:bg-niodark3 h-3"></div>
+        <div className="box-wrapper h-[180px] w-[100%] rounded-3xl bg-transparent border-2 border-base-300 dark:bg-niodark2 p-4 flex items-center justify-center">
+          <div className="">
+            <h1 className="text-4xl font-bold text-textdark4">002</h1>
           </div>
           <div className="tags-section"></div>
         </div>
-        <div className="box-wrapper h-[200px] w-[100%] rounded-2xl bg-white dark:bg-niodark2 p-4">
-          <div className="flex flex-col gap-4">
-            <div className="skeleton dark:bg-niodark3 h-24"></div>
-            <div className="skeleton dark:bg-niodark3 h-3"></div>
-            <div className="skeleton dark:bg-niodark3 h-3"></div>
+        <div className="box-wrapper h-[180px] w-[100%] rounded-3xl bg-transparent border-2 border-base-300 dark:bg-niodark2 p-4 flex items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-textdark4">003</h1>
           </div>
           <div className="tags-section"></div>
         </div>
