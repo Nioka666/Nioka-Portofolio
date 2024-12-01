5e50b7e36368b166a7a7d1b45274985b5ba93ac4
Adhim Niokagi <niokagi@gmail.com>
2024-12-01 13:01:58 +0700

    adjust

--- src/components/react/Projects.jsx
+++ src/components/react/Projects.jsx
@@ -1,28 +1,28 @@
 import PartTitle from "./PartTitle";
 
 function Projects() {
-  function ProjectBox({ st_title, nd_title }) {
+  function ProjectBox({ st_title, nd_title, desc, url }) {
     return (
       <>
         <div
           className="card bg-white dark:bg-gradient-to-b from-niodark3 to-niodark4 max-sm:w-full lg:w-[440px] max-lg:w-[340px] border dark:border-borderbtm">
           <div className="card-body text-left">
-            <h1 className="text-6xl sm:text-7xl">Nio <br /> Nime</h1>
+            <h1 className="text-6xl sm:text-7xl">{st_title} <br /> {nd_title}</h1>
             <div className="tag flex gap-2 mt-2">
               <span className="bg-gray-100 dark:bg-borderbtm px-3 py-1 text-xs rounded-md">React.js</span>
               <span className="bg-gray-100 dark:bg-borderbtm px-3 py-1 text-xs rounded-md">ExpressJS</span>
             </div>
             <p className="mt-4 text-[13px] font-normal dark:text-textdark4">
-              Anime streaming website without ads, with the support of an administration system and allows
-              users subscribtion with several benefits </p>
+              {desc}
+            </p>
             <div className="mt-5 flex justify-between w-full">
               <div className="cursor-pointer">
-                <a href="https://github.com/Nioka666/NioNime">
+                <a href={url}>
                   <i className="bx bxl-github text-xl hover:text-textdark2 duration-200"></i>
                 </a>
               </div>
               <div className="cursor-pointer">
-                <a href="https://github.com/Nioka666/NioNime">
+                <a href={url}>
                   <i
                     className="bx bx-arrow-back rotate-180 text-xl hover:translate-x-1 transition-all duration-200"></i>
                 </a>
@@ -39,9 +39,11 @@ function Projects() {
       <section id="projects" className="projects-component">
         <PartTitle title={"my_projects"} num={"004"} />
         <section className="projects-content">
-          <ProjectBox st_title={"Nio"} nd_title={"Nime"} />
-          <ProjectBox st_title={"Nio"} nd_title={"Libs"} />
-          <ProjectBox st_title={"N"} nd_title={"Quiz"} />
+          <ProjectBox st_title={"Nio"} nd_title={"Nime"} desc={"Anime streaming web without ads, with administration system and allows user subscribtion with several benefits"} url={"https://github.com/Nioka666/NioNime"} />
+
+          <ProjectBox st_title={"Nio"} nd_title={"Libs"} desc={"Dynamic library website with fresh look and administration support"} url={"https://github.com/Nioka666/NioLibs"} />
+
+          <ProjectBox st_title={"N"} nd_title={"Quiz"} desc={"A Simple mobile/android-based quiz app, with a fresh look and fun quiz content"} url={"https://github.com/Nioka666/NQuiz"} />
           <div className="card bg-nioprimary dark:bg-niodark max-sm:w-full sm:w-[410px] text-center h-80">
             <div className="my-auto">
               <i className="bx bxs-ghost text-9xl max-sm:text-[110px] text-gray-300 dark:text-textdark5"></i>
