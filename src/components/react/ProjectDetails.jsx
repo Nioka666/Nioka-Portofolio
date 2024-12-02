bb507c516fb6dbe20cf02fc1fe24bf48adc94151
Adhim Niokagi <niokagi@gmail.com>
2024-12-03 02:12:17 +0700

    adjust

--- src/components/react/ProjectDetails.jsx
+++ src/components/react/ProjectDetails.jsx
@@ -1,7 +1,7 @@
 function ProjectDetails({ url, slug, title, banner_img, date, p1, p2, p3, github_url }) {
     return (
         <>
-            <section className="navigation mt-[-20px] flex justify-between px-[22px] max-sm:mt-[-40px]">
+            <section className="navigation mt-[-20px] flex justify-between px-16 max-sm:mt-[-40px]">
                 <div className="breadcrumbs text-sm">
                     <ul>
                         <li>
@@ -14,11 +14,11 @@ function ProjectDetails({ url, slug, title, banner_img, date, p1, p2, p3, github
                 </div>
                 <div className="blank"></div>
             </section>
-            <section className="project-details-component mt-6 px-5">
+            <section className="project-details-component mt-6 px-16">
                 <header className="grid gap-7">
                     <h1 className="text-5xl max-sm:text-[33px] max-sm:leading-[38px] font-semibold dark:text-textdark3">{title}</h1>
                     <span className="text-gray-500 max-sm:text-sm max-sm:mt-[-10px]">Last update {date}</span>
-                    <div className="rectangle skeleton w-full h-[200px] max-sm:h-[180px] bg-gray-200 dark:bg-niodark3 rounded-xl"></div>
+                    <div className="rectangle skeleton w-full h-[220px] max-sm:h-[180px] bg-gray-200 dark:bg-niodark3 rounded-xl"></div>
                     {/* <img src={banner_img.src} alt="" className="w-full h-[200px] rounded-xl" /> */}
                 </header>
                 <article className="mt-12 max-sm:mt-8 text-left text-[14.8px] sm:text-[17px] [word-spacing:5px] max-sm:[word-spacing:2.5px] leading-[26.5px] text-textdark5 dark:text-textdark4 px-[1px]">
