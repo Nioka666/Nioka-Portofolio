482ffdc0d4b8635951a0af1ef5222af15896f4eb
Adhim Niokagi <niokagi@gmail.com>
2024-12-03 13:42:59 +0700

    adjust

--- src/components/react/SendmailAlert.jsx
+++ src/components/react/SendmailAlert.jsx
@@ -3,7 +3,7 @@ function SendMailAlert() {
     <div
       id="mail-alert"
       role="alert"
-      className="alert fixed left-[50%] top-10 z-[999] hidden w-[82%] -translate-x-1/2 transform border-none transition-all duration-300 ease-linear dark:bg-niodark2 dark:text-nioprimary"
+      className="alert fixed left-[50%] top-10 z-[999] hidden w-[70%] -translate-x-1/2 transform border-none transition-all duration-300 ease-linear dark:bg-niodark2 dark:text-nioprimary"
     >
       <div className="flex items-center gap-3 max-sm:gap-2">
         <i className="bx bxs-check-circle text-green-400"></i>
